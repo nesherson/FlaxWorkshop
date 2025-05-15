843625219e34430b4b37ab63fd1f986a1ff99093
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-05-15 15:54:27 +0200

    Add FPSController

---
+++ Source/Game/FPSController.cs
@@ -0,0 +1,32 @@
+﻿using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// FPSController Script.
+/// </summary>
+public class FPSController : Script
+{
+    public CharacterController CharacterController;
+    public Camera Camera;
+    public float MouseSensitivity;
+
+    private float mouseX;
+    private float mouseY;
+
+    /// <inheritdoc/>
+    public override void OnStart()
+    {
+        Screen.CursorLock = CursorLockMode.Locked;
+        Screen.CursorVisible = false;  
+    }
+
+    /// <inheritdoc/>
+    public override void OnUpdate()
+    {
+        mouseX += Input.GetAxis("Mouse X") * MouseSensitivity;
+        mouseY += Input.GetAxis("Mouse Y") * MouseSensitivity;
+
+        Camera.EulerAngles = new Vector3(mouseY, mouseX, 0);
+    }
+}
