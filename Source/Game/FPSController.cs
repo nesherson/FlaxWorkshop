70a266f24006eb33c8569327518653e16c32ac5f
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-05-16 23:37:04 +0200

    Add basic character movement

--- Source/Game/FPSController.cs
+++ Source/Game/FPSController.cs
@@ -1,4 +1,5 @@
-﻿using FlaxEngine;
+﻿using System;
+using FlaxEngine;
 
 namespace Game;
 
@@ -10,23 +11,47 @@ public class FPSController : Script
     public CharacterController CharacterController;
     public Camera Camera;
     public float MouseSensitivity;
+    public float Speed;
+    public float JumpForce;
 
-    private float mouseX;
-    private float mouseY;
+    private float _gravity = 2000;
+    private float _yVelocity = 0;
+    private float _mouseX;
+    private float _mouseY;
+    private int _jumpCount = 0;
 
     /// <inheritdoc/>
     public override void OnStart()
     {
         Screen.CursorLock = CursorLockMode.Locked;
-        Screen.CursorVisible = false;  
+        Screen.CursorVisible = false;
     }
 
     /// <inheritdoc/>
     public override void OnUpdate()
     {
-        mouseX += Input.GetAxis("Mouse X") * MouseSensitivity;
-        mouseY += Input.GetAxis("Mouse Y") * MouseSensitivity;
+        _mouseX += Input.GetAxis("Mouse X") * MouseSensitivity;
+        _mouseY += Input.GetAxis("Mouse Y") * MouseSensitivity;
 
-        Camera.EulerAngles = new Vector3(mouseY, mouseX, 0);
+        _mouseY = Math.Clamp(_mouseY, -90, 90);
+
+        Camera.EulerAngles = new Vector3(_mouseY, _mouseX, 0);
+    }
+
+    public override void OnFixedUpdate()
+    {
+        var moveInput = new Vector3
+        {
+            X = Input.GetAxis("Horizontal"),
+            Z = Input.GetAxis("Vertical")
+        };
+
+        moveInput.Normalize();
+
+        var lookRotation = new Vector3(0, Camera.LocalEulerAngles.Y, 0);
+        var direction = Vector3.Transform(moveInput, Quaternion.Euler(lookRotation));
+        var velocity = direction * Speed * Time.DeltaTime;
+        
+        CharacterController.Move(velocity);
     }
 }
